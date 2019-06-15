7ece6daf24f08ddb4bf6701abe10368549dfeeda
Wynn <2601659975@qq.com>
2019-06-15 14:17:54 +0800

    38 报数

--- 38.报数.java
+++ 38.报数.java
@@ -11,12 +11,37 @@
  * 数字 4 的报数：根据上一个报数得到，即 对数字 3 的报数进行报数得到 一个二，一个一，即 1211
  * 数字 5 的报数：根据上一个报数得到，即 对数字 4 的报数进行报数得到 一个一，一个二，两个一，即111221
  * 数字 6 的报数：根据上一个报数得到，即 对数字 5 的报数进行报数得到 三个一，两个二，一个一，即312211
+ * √ Your runtime beats 38.76 % of java submissions
+ * √ Your memory usage beats 75.58 % of java submissions (36.1 MB)
  */
 class Solution {
     public String countAndSay(int n) {
-        String say = "1";
-        for (int i = 1; i <= n; i++) {
-
+        if (n == 1)
+            return "1";
+        String say = "11";
+        StringBuffer sb = new StringBuffer();
+        for (int i = 2; i < n; i++) { // 用循环递推报数
+            // sb 作为存储每次推导结果的容器，所以在循环开始需要清空一下
+            int sb_len = sb.length();
+            sb.delete(0, sb_len);
+            int len = say.length(); // 为防止多次调用 length() 影响效率，故单独提取出来，算是小的优化（苦笑）
+            int repeat = 0; // 标识数字重复出现的次数
+            for (int j = 0; j < len; j++) {
+                char current = say.charAt(j); // 比较位-当前字符
+                char next; // 比较位-下一个字符
+                if (j != len - 1)
+                    next = say.charAt(j + 1);
+                else
+                    next = '0'; // 避免下标越界异常和空指针异常，需要给next 赋值，由题目可知，0 是不错的选择
+                if (current == next) {
+                    repeat++;
+                } else {
+                    sb.append(String.valueOf(repeat + 1) + current);
+                    repeat = 0; // 每一次比较位变换，计数器都要归零
+                }
+            }
+            say = sb.toString();
         }
+        return say;
     }
 }
